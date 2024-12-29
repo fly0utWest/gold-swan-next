6cb286869f6fe5ff566f5942f40bb0c9b87fc93b
Nikita Ablamskiy <shealtov@gmail.com>
2024-12-29 20:13:52 +0300

    project card added

---
+++ src/shared/ui/project-card.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Heading from "@/shared/ui/heading";
+
+interface ProjectCardProps {
+  icon?: React.ReactNode;
+  title: string;
+  description: string;
+  features: string[];
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  icon,
+  title,
+  description,
+  features,
+}) => {
+  return (
+    <div className="flex flex-col lg:flex-row gap-4 bg-background p-6 rounded-3xl border-2 border-neutral-800 max-w-screen-xl">
+      <div className="lg:w-1/2">
+        <div className="flex flex-col gap-4 items-center lg:items-start">
+          <span className="w-[20px] lg:w-full border-2 border-primary-200"></span>
+          {icon}
+          <Heading as="h3" className="text-3xl font-bold">
+            {title}
+          </Heading>
+        </div>
+        <p className="text-xl text-center lg:text-left mt-4">{description}</p>
+      </div>
+      <div className="lg:w-1/2 mt-4 lg:mt-0">
+        <Heading as="h4" className="text-2xl font-semibold mb-4">
+          Key Features
+        </Heading>
+        <ul className="list-disc list-inside space-y-2">
+          {features.map((feature, index) => (
+            <li key={index} className="text-lg marker:text-primary-500">
+              {feature}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
