13df1ea884e7ee1541616be6e0c6b50aaa91bd04
nikita f0w <shealtov@gmail.com>
2025-01-27 19:56:14 +0300

    width of marquee fix

--- src/widgets/home-page/partners-marquee.tsx
+++ src/widgets/home-page/partners-marquee.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 
 const PartnersMarquee: React.FC = () => {
     return (
-    <div className="p-6 bg-card overflow-hidden border-y-primary-200 border-y-2">
+    <div className="p-6 bg-card overflow-hidden border-y-primary-500 border-y-2">
       <StaticMarquee repeatCount={2}>
         {partnerMarquee.map((image, index) => (
-          <div key={index} className="flex items-center justify-center">
+          <div key={index} className="flex h-max items-center justify-center">
             <Image
               className="mx-4"
               src={image}
