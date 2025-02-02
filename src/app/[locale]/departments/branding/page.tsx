59241ed0dccf109b3b5525a14de8448a5432eca0
nikita f0w <shealtov@gmail.com>
2025-02-02 10:50:03 +0300

    brand dev department is ready

--- src/app/[locale]/departments/branding/page.tsx
+++ src/app/[locale]/departments/branding/page.tsx
@@ -1,118 +1,118 @@
 import { useTranslations } from "next-intl";
 import AboutDepartment from "@/widgets/departments/about-department";
-import DepartmentOverview from "@/widgets/departments/department-overview";
-import DepartmentTools from "@/widgets/departments/department-tools";
-import { AlignLeft } from "solar-icon-set";
-import StrategySteps from "@/widgets/departments/strategy-steps";
-import SectionDelimeter from "@/shared/ui/section-delimeter";
 import Button from "@/shared/ui/button";
 import Link from "@/shared/ui/link";
-import { StarRainbow } from "solar-icon-set";
+import Marquee from "@/shared/ui/marquee";
+import { Star2 } from "solar-icon-set";
+import DepartmentHeroSection from "@/widgets/departments/department-hero-section.";
+import ServiceCard from "@/widgets/departments/service-card";
+import DepartmentServices from "@/widgets/departments/department-services";
+import * as brandServices from "@/shared/models/departments/brandDev";
+
 
 export default function BrandDevelopmentPage() {
-  const t = useTranslations("departments.brandDevelopment");
-
-  const benefits = [
-    "strongBrandIdentity",
-    "enhancedPublicImage",
-    "longTermSuccess",
-  ];
-
-  const tools = [
-    "brandBook",
-    "brandPositioning",
-    "uspDevelopment",
-    "creativeAdvertising",
-    "prAndMediaRelations",
-    "influencerMarketing",
-    "eventOrganization",
-    "crisisPRManagement",
-  ];
-
-  const strategies = [
-    {
-      key: "contentStrategy",
-      steps: [
-        "research",
-        "design",
-        "documentation",
-        "audienceResearch",
-        "valueMapping",
-        "strategyImplementation",
-      ],
-    },
-    {
-      key: "positioning",
-      steps: ["defineUVP", "keyMessaging", "competitorAnalysis"],
-    },
-    {
-      key: "usp",
-      steps: ["featureAnalysis", "targetedMessaging", "proofPoints"],
-    },
-    {
-      key: "advertisingCampaigns",
-      steps: [
-        "ideaGeneration",
-        "contentProduction",
-        "crossPlatformDistribution",
-        "performanceAnalysis",
-      ],
-    },
-    {
-      key: "prRelations",
-      steps: ["messageCrafting", "distribution", "followUp"],
-    },
-    {
-      key: "influencerCampaigns",
-      steps: ["research", "collaboration", "analysis"],
-    },
-    {
-      key: "eventPlanning",
-      steps: ["conceptCreation", "execution", "followUp"],
-    },
-    {
-      key: "crisisManagement",
-      steps: ["situationAnalysis", "responseStrategy", "monitoring"],
-    },
-  ];
+  const t = useTranslations("departments.brandDevelopmentAndPR");
+  const miscT = useTranslations("misc");
 
   return (
     <>
-      <AboutDepartment heading={t("title")} description={t("description")} />
-      <SectionDelimeter />
-      <DepartmentOverview
-        heading={t("overview.title")}
-        definition={t("overview.definition")}
-        benefits={benefits.map((benefit) => t(`overview.benefits.${benefit}`))}
+      <DepartmentHeroSection
+        Icon={Star2}
+        heading={[
+          t("title.firstString"),
+          t("title.secondString"),
+          t("title.thirdString"),
+        ]}
       />
-      <SectionDelimeter />
-      <DepartmentTools
-        heading={t("tools.title")}
-        tools={tools.map((tool) => ({
-          Icon: AlignLeft,
-          heading: {
-            headingLevel: "h3",
-            headingText: t(`tools.${tool}.title`),
-          },
-          description: t(`tools.${tool}.description`),
-        }))}
+      <AboutDepartment
+        verticalMargin={16}
+        heading={t("title.full")}
+        description={t("description")}
       />
-      <SectionDelimeter />
-      {strategies.map((strategy) => (
-        <div key={strategy.key}>
-          <StrategySteps
-            Icon={StarRainbow}
-            heading={t(`strategies.${strategy.key}.title`)}
-            steps={strategy.steps.map((step) =>
-              t(`strategies.${strategy.key}.steps.${step}`)
-            )}
-            example={t(`strategies.${strategy.key}.example`)}
+      <DepartmentServices title={t("brandBookCreation.title")}>
+        {brandServices.brandBookServices.map((service, index) => (
+          <ServiceCard
+            key={index}
+            Icon={service.Icon}
+            title={t(service.title)}
+            description={t(service.description)}
           />
-          <SectionDelimeter />
-        </div>
-      ))}
-      <Link href="/contact" className="block w-fit mx-auto mt-16">
-        <Button className="w-48 px-[0!important]">CONTACT US</Button>
+        ))}
+      </DepartmentServices>
+      <DepartmentServices rounded title={t("brandPositioning.title")}>
+        {brandServices.brandPositioningServices.map((service, index) => (
+          <ServiceCard
+            key={index}
+            Icon={service.Icon}
+            title={t(service.title)}
+            description={t(service.description)}
+          />
+        ))}
+      </DepartmentServices>
+      <DepartmentServices title={t("uspDevelopment.title")}>
+        {brandServices.uspDevelopmentServices.map((service, index) => (
+          <ServiceCard
+            key={index}
+            Icon={service.Icon}
+            title={t(service.title)}
+            description={t(service.description)}
+          />
+        ))}
+      </DepartmentServices>
+      <DepartmentServices rounded title={t("creativeAdvertising.title")}>
+        {brandServices.creativeAdvertisingServices.map((service, index) => (
+          <ServiceCard
+            key={index}
+            Icon={service.Icon}
+            title={t(service.title)}
+            description={t(service.description)}
+          />
+        ))}
+      </DepartmentServices>
+      <DepartmentServices title={t("prAndMediaRelations.title")}>
+        {brandServices.prAndMediaRelationsServices.map((service, index) => (
+          <ServiceCard
+            key={index}
+            Icon={service.Icon}
+            title={t(service.title)}
+            description={t(service.description)}
+          />
+        ))}
+      </DepartmentServices>
+      <DepartmentServices rounded title={t("influencerMarketing.title")}>
+        {brandServices.influencerMarketingServices.map((service, index) => (
+          <ServiceCard
+            key={index}
+            Icon={service.Icon}
+            title={t(service.title)}
+            description={t(service.description)}
+          />
+        ))}
+      </DepartmentServices>
+      <DepartmentServices title={t("eventOrganization.title")}>
+        {brandServices.eventOrganizationServices.map((service, index) => (
+          <ServiceCard
+            key={index}
+            Icon={service.Icon}
+            title={t(service.title)}
+            description={t(service.description)}
+          />
+        ))}
+      </DepartmentServices>
+      <DepartmentServices rounded title={t("crisisPrManagement.title")}>
+        {brandServices.crisisPrManagementServices.map((service, index) => (
+          <ServiceCard
+            key={index}
+            Icon={service.Icon}
+            title={t(service.title)}
+            description={t(service.description)}
+          />
+        ))}
+      </DepartmentServices>
+      <Link href="/contact" className="flex justify-center mt-10">
+        <Button className="overflow-hidden uppercase w-48 word-spacing-6 px-[0!important]">
+          <Marquee repeatCount={2}>{miscT("contactUs")}</Marquee>
+        </Button>
       </Link>
     </>
   );
