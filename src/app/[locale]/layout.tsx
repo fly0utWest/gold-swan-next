c26117e7199838cb340a7cd94c1c43c3d98783d4
nikita f0w <shealtov@gmail.com>
2025-02-05 11:38:45 +0300

    poopoo

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -40,28 +40,6 @@ export default async function RootLayout(
           src="https://staticinchatai.5dgo.dev/inchat-widget.iife.js"
           strategy="beforeInteractive"
         />
-        {process.env.GOOGLE_ANALYTICS_ID && (
-          <>
-            <Script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-            />
-            <Script
-              id="google-analytics"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-              }}
-            />
-          </>
-        )}
       </head>
       <body className={`${oswaldSans.className}`}>
         {process.env.GOOGLE_ANALYTICS_ID && (
