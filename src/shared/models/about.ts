a13f06101dfa8f073212837f6727f8c6afe55753
nikita f0w <shealtov@gmail.com>
2025-02-05 11:35:16 +0300

    refactor array

---
+++ src/shared/models/about.ts
@@ -0,0 +1,35 @@
+import {
+  Lightbulb,
+  Scale,
+  ShieldCheck,
+  Wallet,
+  ClockCircle,
+} from "solar-icon-set";
+
+export const aboutCards = [
+  {
+    title: "flexibility.translated",
+    content: "flexibility.explained",
+    icon: Lightbulb,
+  },
+  {
+    title: "speed.translated",
+    content: "speed.explained",
+    icon: ClockCircle,
+  },
+  {
+    title: "confidentiality.translated",
+    content: "confidentiality.explained",
+    icon: ShieldCheck,
+  },
+  {
+    title: "pricing.translated",
+    content: "pricing.explained",
+    icon: Wallet,
+  },
+  {
+    title: "ethicsAndResponsibility.translated",
+    content: "ethicsAndResponsibility.explained",
+    icon: Scale,
+  },
+];
