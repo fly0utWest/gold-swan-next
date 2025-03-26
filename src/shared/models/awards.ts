ac54010280e41159aba567839504f9620864b5f5
nikita ablamskiy <shealtov@gmail.com>
2025-03-26 12:54:11 +0300

    png to webp convert

--- src/shared/models/awards.ts
+++ src/shared/models/awards.ts
@@ -2,17 +2,17 @@ export const awards = [
   {
     title: "bestDataSolutionsPlatform.title",
     description: "bestDataSolutionsPlatform.description",
-    image: "/images/logos/1.png",
+    image: "/images/logos/1.webp",
   },
   {
     title: "top5.title",
     description: "top5.description",
-    image: "/images/logos/2.png",
+    image: "/images/logos/2.webp",
   },
   {
     title: "reputationHouse.title",
     description: "reputationHouse.description",
-    image: "/images/logos/2.png",
+    image: "/images/logos/2.webp",
   },
   {
     title: "top20.title",
@@ -22,13 +22,13 @@ export const awards = [
   {
     title: "reputationDeptGoldSwanAI.title",
     description: "reputationDeptGoldSwanAI.description",
-    image: "/images/logos/3.png",
+    image: "/images/logos/3.webp",
     large: true,
   },
   {
     title: "reputationDeptGoldSwanInnovation.title",
     description: "reputationDeptGoldSwanInnovation.description",
-    image: "/images/logos/3.png",
+    image: "/images/logos/3.webp",
     large: true,
   },
 ];
@@ -39,29 +39,29 @@ export const awardChevrons = [
   "/images/awards/3.webp",
   "/images/awards/4.webp",
   "/images/awards/5.webp",
-  "/images/awards/6.png",
-  "/images/awards/7.png",
+  "/images/awards/6.webp",
+  "/images/awards/7.webp",
 ];
 
 export const partnerLogos = [
-  "/images/logos/acronis.png",
-  "/images/logos/danone.png",
-  "/images/logos/benz.png",
-  "/images/logos/gostudy.png",
-  "/images/logos/kia.png",
-  "/images/logos/melvita.png",
-  "/images/logos/mediamarkt.png",
-  "/images/logos/enplus.png",
+  "/images/logos/acronis.webp",
+  "/images/logos/danone.webp",
+  "/images/logos/benz.webp",
+  "/images/logos/gostudy.webp",
+  "/images/logos/kia.webp",
+  "/images/logos/melvita.webp",
+  "/images/logos/mediamarkt.webp",
+  "/images/logos/enplus.webp",
 ];
 
 export const partnerMarquee = [
-  "/images/logos/zawya.png",
-  "/images/logos/yahoo.png",
-  "/images/logos/benzinga.png",
-  "/images/logos/khaleej.png",
-  "/images/logos/gulfNews.png",
-  "/images/logos/digitalJournal.png",
-  "/images/logos/bloomberg.png",
-  "/images/logos/arabiya.png",
-  "/images/logos/ap.png",
+  "/images/logos/zawya.webp",
+  "/images/logos/yahoo.webp",
+  "/images/logos/benzinga.webp",
+  "/images/logos/khaleej.webp",
+  "/images/logos/gulfNews.webp",
+  "/images/logos/digitalJournal.webp",
+  "/images/logos/bloomberg.webp",
+  "/images/logos/arabiya.webp",
+  "/images/logos/ap.webp",
 ];
