ccd3249aec7e4c525d4ee8268730d35717264e86
Paper Light <kwazzartcreature@gmail.com>
2025-06-16 23:43:08 +0300

    autocapturing as docs says

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -3,4 +3,5 @@ import posthog from "posthog-js";
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   capture_pageview: "history_change",
+  defaults: "2025-05-24",
 });
