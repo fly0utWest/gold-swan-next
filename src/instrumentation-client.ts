d9510d9bbcc8aa7ea0865baf60b17f48462a2bfc
Paper Light <kwazzartcreature@gmail.com>
2025-06-16 19:56:27 +0300

    fix posthog

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,10 +1,6 @@
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   capture_pageview: "history_change",
-  capture_pageleave: true, // Enable pageleave capture
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-  debug: process.env.NODE_ENV === "development",
 });
