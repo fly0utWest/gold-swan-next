97b4b3e741c4fdfd148abc2cad130c87a8a03854
Paper Light <kwazzartcreature@gmail.com>
2025-06-17 00:24:04 +0300

    autocapturing GO

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -4,4 +4,5 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   capture_pageview: "history_change",
   defaults: "2025-05-24",
+  autocapture: true,
 });
